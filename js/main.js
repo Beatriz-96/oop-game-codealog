d3eb2d1329f79c19622ea6687326851f476d070a
Beatriz <beatrizrmb@outlook.com>
2023-08-10 15:37:41 +0100

    create and move obstacles

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Player {
     constructor () {
         this.positionX = 50;
-        this.positionY = 50;
-        this.width = 4;
-        this.height= 3;
+        this.positionY = 0;
+        this.width = 20;
+        this.height= 10;
         this.domElement = null;
 
         this.createDomElement();
@@ -32,8 +32,55 @@ class Player {
         }
     
 
+class Obstacle {
+    constructor (){
+        this.positionX = 50;
+        this.positionY = 100;
+        this.width = 20;
+        this.height= 5;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        this.domElement = document.createElement("div");
+
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+    }
+    moveDown(){
+        this.positionY -= 2;
+        this.domElement.style.bottom = this.positionY + "vh";
+
+
+    }
+}
+
 
 const player = new Player();
+
+const obstaclesArr = [];
+
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle);
+    console.log("we've created a new obstacle...", obstaclesArr.length);  
+}, 4000)
+
+
+
+setInterval(() => {
+    obstaclesArr.forEach( (obstacleInstance) => {
+        obstacleInstance.moveDown();
+    });
+}, 200);
+
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowLeft") {
