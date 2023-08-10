26a1ddbc8281bc8d04e36efd050a05b6e9a5cc0a
Beatriz <beatrizrmb@outlook.com>
2023-08-10 13:44:31 +0100

    implement funcionality to display and move player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-console.log("hello world")
+class Player {
+    constructor () {
+        this.positionX = 50;
+        this.positionY = 50;
+        this.width = 4;
+        this.height= 3;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        this.domElement = document.createElement("div");
+
+        this.domElement.id = "player";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+
+        const parentElm  = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+    }
+
+    moveLeft(){
+            this.positionX--;  
+            this.domElement.style.left = this.positionX + "vw";
+        } 
+    moveRight(){
+            this.positionX++; 
+            this.domElement.style.left = this.positionX + "vw";
+        } 
+        }
+    
+
+
+const player = new Player();
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+        player.moveLeft();
+    } else if (event.key === "ArrowRight") {
+        player.moveRight();
+    }
+});
+
+
+
+
